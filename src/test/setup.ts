005f661a4945ceeeb1adffe3a3975fc2df46fdfc
Ha22yX <kicofy@vip.qq.com>
2026-08-03 08:31:41 +0800

    test: provide ResizeObserver fallback

--- src/test/setup.ts
+++ src/test/setup.ts
@@ -1,1 +1,17 @@
 import "@testing-library/jest-dom/vitest";
+
+class TestResizeObserver implements ResizeObserver {
+  disconnect(): void {}
+
+  observe(): void {}
+
+  unobserve(): void {}
+}
+
+if (typeof globalThis.ResizeObserver === "undefined") {
+  Object.defineProperty(globalThis, "ResizeObserver", {
+    configurable: true,
+    value: TestResizeObserver,
+    writable: true,
+  });
+}
